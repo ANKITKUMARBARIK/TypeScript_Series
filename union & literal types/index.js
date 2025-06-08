f1436ed352ef98d7e94f734f360dbfa6746ebceb
ANKITKUMARBARIK <barikankitkumar2@gmail.com>
2025-06-08 20:10:59 +0530

    add union and literal types examples

---
+++ union & literal types/index.js
@@ -0,0 +1,17 @@
+"use strict";
+// union
+let resorPri;
+resorPri = 13;
+resorPri = "lion";
+resorPri = true;
+const resOrArr = [
+    "camp",
+    "TypeScript",
+    500,
+    20,
+    "jupyter",
+];
+// literal
+let resLiteral = "404";
+resLiteral = "201";
+resLiteral = "404";
---
+++ union & literal types/index.ts
@@ -0,0 +1,18 @@
+// union
+let resorPri: number | string | boolean;
+resorPri = 13;
+resorPri = "lion";
+resorPri = true;
+
+const resOrArr: (number | string)[] = [
+    "camp",
+    "TypeScript",
+    500,
+    20,
+    "jupyter",
+];
+
+// literal
+let resLiteral: "201" | "404" = "404";
+resLiteral = "201";
+resLiteral = "404";
