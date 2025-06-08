9d3b99653ce7a0a62d6d9ba4bb63888ec21443cb
ANKITKUMARBARIK <barikankitkumar2@gmail.com>
2025-06-08 20:10:21 +0530

    add basic array and tuple examples

---
+++ arrays & tuples/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let arr = [1, 2, 3, "black", "white"];
+let arrObj = [10, 20, 30, { name: "admin" }, { name: 40 }];
+// array
+let restrictArr = [100, 200, 300, 400, 500];
+let restrictStr = ["100", "200", "300", "400", "500"];
+// tuple
+let restrictTup = ["black", 100, 200, true];
---
+++ arrays & tuples/index.ts
@@ -0,0 +1,10 @@
+let arr = [1, 2, 3, "black", "white"];
+
+let arrObj = [10, 20, 30, { name: "admin" }, { name: 40 }];
+
+// array
+let restrictArr: number[] = [100, 200, 300, 400, 500];
+let restrictStr: string[] = ["100", "200", "300", "400", "500"];
+
+// tuple
+let restrictTup: [string, number, number, boolean] = ["black", 100, 200, true];
